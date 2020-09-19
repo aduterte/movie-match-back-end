9bab59f8396c2c3905135ab4b720fe6819bf522a
aduterte <alexduterte@gmail.com>
2020-09-19 16:05:26 -0400

    added JWT auth to user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::API
+
+    def secret_key
+        "itsamatch"
+    end
+  
+    #given a payload {user_id: ?} => token
+    def encode(payload)
+      JWT.encode(payload, secret_key, 'HS256')
+    end
+  
+    #given a token "abc.123" => payload {user_id: ?}
+    def decode(token)
+      JWT.decode(token, "itsamatch", true, {algorithm: "HS256"})[0]
+    end
+    
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,52 @@
 class UsersController < ApplicationController
+
+    def show
+
+        user = User.find(params[:id])
+        if user 
+            render json: user.as_json(include: [:movies], except: [:created_at, :updated_at])
+        else
+            render json: {error_message:  'User not found'}
+        end
+    end
+
+    def create
+        # byebug
+        user = User.create(username: params[:username], email: params[:email], password: params[:password], password_confirmation: params[:password_confirmation])
+        if user.valid?
+            user.save
+            payload = {user_id: user.id}
+            token = encode(payload)
+            
+            render :json => {user: user.as_json(include: [:movies], except: [:created_at, :updated_at]), token: token}
+        else
+            render json: user.errors.full_messages
+        end
+    end
+
+    def update
+        user = User.find(params[:id])
+        render json: user
+    end
+
+    def login 
+        user = User.find_by(email: params[:email])
+        
+        if user && user.authenticate(params[:password])
+            payload = {user_id: user.id}
+            token = encode(payload)
+            
+            render :json => {user: user.as_json(include: [:movies], except: [:created_at, :updated_at]), token: token}
+        else 
+            render json: {error_message: "User not found"}
+        end
+    end
+
+    def token_authenticate
+        
+        token = request.headers["Authenticate"]
+        user = User.find(decode(token)["user_id"])
+        render json: user.as_json(include: [:movies], except: [:created_at, :updated_at])
+        
+    end
 end
